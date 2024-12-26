bac22e9ca18c110208fadf6685b6c0c3707727d9
IgnjatMarko <ignjatovicmarko2023@gmail.com>
2024-12-26 17:27:11 +0100

    Vercel configuration

--- astroblog/astro.config.mjs
+++ astroblog/astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/server';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
+  adapter: vercel(),
 });
